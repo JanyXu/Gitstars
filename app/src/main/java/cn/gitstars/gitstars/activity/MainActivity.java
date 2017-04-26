b12cfc43343f4f090927c04f24b0beef3850a852
xuxiaojin <xxj9216571>
2017-04-26 17:01:33 +0800

    gitstar

--- app/src/main/java/cn/gitstars/gitstars/activity/MainActivity.java
+++ app/src/main/java/cn/gitstars/gitstars/activity/MainActivity.java
@@ -4,24 +4,25 @@ import android.graphics.Color;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import cn.gitstars.gitstars.R;
 import cn.gitstars.gitstars.base.BaseActivity;
 import cn.gitstars.gitstars.presenter.MainPresenter;
+import cn.gitstars.gitstars.utils.ToastUtil;
 
 /**
  * Created by JanyXu on 2017/04/25.
  * 首页
  */
-public class MainActivity extends BaseActivity<MainPresenter> {
+public class MainActivity extends BaseActivity<MainPresenter> implements View.OnClickListener {
 
     @InjectView(R.id.toolbar)
     Toolbar toolbar;
@@ -31,6 +32,7 @@ public class MainActivity extends BaseActivity<MainPresenter> {
     NavigationView navigation_start;
     @InjectView(R.id.navigation_end)
     NavigationView navigation_end;
+    Button btn_close;
 
     @Override
     protected MainPresenter loadPresenter() {
@@ -50,36 +52,30 @@ public class MainActivity extends BaseActivity<MainPresenter> {
     @Override
     protected void initView() {
         ButterKnife.inject(this);
+        //ButterKnife.inject(this,navigation_end.getHeaderView(0));
+//        btn_close = ButterKnife.findById(navigation_end.getHeaderView(0), R.id.btn_close);
         toolbar.setTitleTextColor(Color.WHITE);
-        //设置toolbar标题文本
         toolbar.setTitle("首页");
         toolbar.setOnCreateContextMenuListener(this);
-        //设置toolbar
         setSupportActionBar(toolbar);
         //设置左上角图标是否可点击
 //        getSupportActionBar().setHomeButtonEnabled(true);
 //        //左上角加上一个返回图标
 //        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//        //初始化ActionBarDrawerToggle(ActionBarDrawerToggle就是一个开关一样用来打开或者关闭drawer)
-//        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawer_layout, toolbar, R.string.openString, R.string.closeString) {
-//            /*
-//            * 抽屉菜单打开监听
-//            * */
-//            @Override
-//            public void onDrawerOpened(View drawerView) {
-//                Toast.makeText(MainActivity.this, "菜单打开了", Toast.LENGTH_SHORT).show();
-//                super.onDrawerOpened(drawerView);
-//            }
-//
-//            /*
-//            * 抽屉菜单关闭监听
-//            * */
-//            @Override
-//            public void onDrawerClosed(View drawerView) {
-//                Toast.makeText(MainActivity.this, "菜单关闭了", Toast.LENGTH_SHORT).show();
-//                super.onDrawerClosed(drawerView);
-//            }
-//        };
+        navigation_end.setNavigationItemSelectedListener(
+
+                new NavigationView.OnNavigationItemSelectedListener() {
+
+                    private MenuItem mPreMenuItem;
+
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        if (mPreMenuItem != null) mPreMenuItem.setChecked(false);
+
+                        return true;
+                    }
+                });
+        //btn_close.setOnClickListener(this);
     }
 
     @Override
@@ -89,7 +85,11 @@ public class MainActivity extends BaseActivity<MainPresenter> {
 
     @Override
     protected void otherViewClick(View view) {
-
+        switch (view.getId()) {
+            case R.id.btn_close:
+                ToastUtil.setToast("ggg");
+                break;
+        }
     }
 
     @Override
